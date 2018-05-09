2666c2be69528c9a6ff8741748108bdb191abda9
jgleyzes <j.gleyzes@gmail.com>
2018-05-08 19:06:40 -0700

    Corrected window function

--- MCMC_clean.py
+++ MCMC_clean.py
@@ -96,20 +96,23 @@ def get_grid(gridname,nbinsAs=100,nbins = 50,withBisp=False):
 
     TablePlin = np.load(opa.abspath(opa.join(INPATH,'GridsEFT/TablePlin%s.npy'%gridname)))
     TablePloop = np.load(opa.abspath(opa.join(INPATH,'GridsEFT/TablePloop%s.npy'%gridname)))
+    Tablesigsq = np.load(opa.abspath(opa.join(INPATH,'GridsEFT/Tablesigsq%s.npy'%gridname)))
 
     Plininterp = scipy.interpolate.RegularGridInterpolator((lnAstab,Omtab,htab),TablePlin.reshape((nbinsAs,nbins,nbins,TablePlin.shape[-2],TablePlin.shape[-1])))
     Ploopinterp = scipy.interpolate.RegularGridInterpolator((lnAstab,Omtab,htab),TablePloop.reshape((nbinsAs,nbins,nbins,TablePloop.shape[-2],TablePloop.shape[-1])))
-    interpolations = [Plininterp,Ploopinterp]
+    Sigsqinterp = scipy.interpolate.RegularGridInterpolator((lnAstab,Omtab,htab),Tablesigsq.reshape((nbinsAs,nbins,nbins)))
+    
+    interpolations = [Plininterp,Ploopinterp,Sigsqinterp]
     if withBisp:
         TableBisp = np.load(opa.abspath(opa.join(INPATH,'GridsEFT/TableBisp%s.npy'%gridname)))
         Bispinterp = scipy.interpolate.RegularGridInterpolator((lnAstab,Omtab,htab),TableBisp.reshape((nbinsAs,nbins,nbins,TableBisp.shape[-2],TableBisp.shape[-1])))
-        interpolations = [Plininterp,Ploopinterp,Bispinterp]
+        interpolations = [Plininterp,Ploopinterp,Sigsqinterp,Bispinterp]
         
     return lnAsmin,lnAsmax,Ommin,Ommax,hmin,hmax,interpolations
     
     
     
-def computePS(cvals,datalin,dataloop,setkin,setkout,removesqsig=True):
+def computePS(cvals,datalin,dataloop,setkin,setkout,sigsq=-100):
     
     """ Computes the power spectra given the b_i and the EFT power spectra
 
@@ -139,10 +142,9 @@ def computePS(cvals,datalin,dataloop,setkin,setkout,removesqsig=True):
         setkin = setkin[:len(setkin)/3]
     if check_if_multipoles_k_array(setkout):
         setkout = setkin[:len(setkout)/3]
-    P11int =  interp1d(setkin, datalin[0,-1])
-    sigsq = 2*scipy.integrate.quad(lambda x: x**2/(2*np.pi)**2*(P11int(x))**2,setkin.min(),setkin.max())[0]      
+         
         
-    P0 = interp1d(setkin,np.dot(cvals,data0)+datalin0[0]+b1*datalin0[1]+b1*b1*datalin0[2] - 2*removesqsig*(-b1 + b2 + b4)**2*sigsq)(setkout)
+    P0 = interp1d(setkin,np.dot(cvals,data0)+datalin0[0]+b1*datalin0[1]+b1*b1*datalin0[2] - 2*(-b1 + b2 + b4)**2*sigsq)(setkout)
     P2 = interp1d(setkin,np.dot(cvals,data2)+datalin2[0]+b1*datalin2[1]+b1*b1*datalin2[2])(setkout)
     P4 = interp1d(setkin,np.dot(cvals,data4)+datalin4[0]+b1*datalin4[1]+b1*b1*datalin4[2])(setkout)
     
@@ -279,9 +281,9 @@ def lnlike(theta, xdata, ydata, Cinv, free_para, fix_para,bounds,fiducial, inter
         
     # Import the power spectra interpolators on the grid
         if withBisp:
-            Plininterp,Ploopinterp,Bispinterp = interpolation_grid
+            Plininterp,Ploopinterp,Sigsqinterp,Bispinterp = interpolation_grid
         else:
-            Plininterp,Ploopinterp = interpolation_grid 
+            Plininterp,Ploopinterp,Sigsqinterp = interpolation_grid 
             Bispinterp = None
         
         kfull = Ploopinterp((lnAs,Om,h))[:,0]
@@ -291,27 +293,37 @@ def lnlike(theta, xdata, ydata, Cinv, free_para, fix_para,bounds,fiducial, inter
         
         Ploop = np.swapaxes(Ploopinterp((lnAs,Om,h)).reshape(3,len(kfull),22),axis1 = 1,axis2 = 2)[:,1:,:]
         Plin = np.swapaxes(Plininterp((lnAs,Om,h)).reshape(3,len(kfull),4),axis1 = 1,axis2 = 2)[:,1:,:]       
-      
+        
+        # Get sigma^2 to be removed
+        
+        sigsq = Sigsqinterp((lnAs,Om,h))  
      
         # Compute the PS       
         valueb = np.array([b1,b2,b3,b4,b5,b6,b7,b8,b9,b10])        
-        Pmodel = computePS(valueb,Plin,Ploop,kfull,kfull)
+        Pmodel_original = computePS(valueb,Plin,Ploop,kfull,kfull,sigsq=sigsq)
+        Pmodel = Pmodel_original.copy()
     
         #The AP parameters
         qperp,qpar = get_AP_param(Om,h,fiducial)
+        
+       
     
         if not binning:
-            Pmodel = APpowerspectraNkmu.changetoAPnobinning(Pmodel,kfull,xdata,qperp,qpar)
+            Pmodel = APpowerspectraNkmu.changetoAPnobinning(Pmodel,kfull,kfull,qperp,qpar)
         else:
             if type(TableNkmu) == type(None):
                 raise Exception('You want to account for binning but forgot to provide a TableNkmu (array of shape (3,n)) obtained from the sims/ Can be found in input/TableNkmu')
-            else : Pmodel = APpowerspectraNkmu.changetoAPbinning(Pmodel,kfull,xdata,qperp,qpar,TableNkmu)
+            else : Pmodel = APpowerspectraNkmu.changetoAPbinning(Pmodel,kfull,kfull,qperp,qpar,TableNkmu)
     
     
         if window:
             if type(dataQ) ==  type(None):
                 raise Exception('You want to account for window function but forgot to provide a dataQ (array of shape (8,n)) obtained from the sims. Can be found in input/dataQ')
-            else: Pmodel = WindowFFTlog.transformQ(np.concatenate(Pmodel),xdata,xdata,dataQ,extrap = False)
+            else: 
+                kjunhigh = 0.75
+                Pmodel = WindowFFTlog.transformQ(np.concatenate(Pmodel),np.concatenate([kfull,kfull,kfull]),xdata,dataQ,kr=0.5,extrap=True,setkextrap= 10**(np.linspace(-5,np.log10(2*kjunhigh),200)),k_junc_low=kfull[1],k_junc_high=kjunhigh,ktr=2,sig=0.5,withlog=False,damp=False)
+        else:
+            Pmodel = APpowerspectraNkmu.changetoAPnobinning(Pmodel,kfull,xdata,1,1) #This is just to interpolate the power spectrum on xdata
     
         
         modelX = np.concatenate(Pmodel)
@@ -333,10 +345,16 @@ def lnlike(theta, xdata, ydata, Cinv, free_para, fix_para,bounds,fiducial, inter
     
         diff  =  (modelX - ydata)
         step1 = np.dot(Cinv,diff)
-        
-        
         chi2 = np.dot(diff,step1)
-        
+        if np.isnan(chi2):
+            modelX = np.concatenate(APpowerspectraNkmu.changetoAPnobinning(Pmodel_original,kfull,xdata,qperp,qpar))
+            if withBisp:
+                modelX = np.concatenate([modelX,Bisp[masktriangle]])
+            diff  =  (modelX - ydata)
+            step1 = np.dot(Cinv,diff)
+            chi2 = np.dot(diff,step1)
+            #print('chi2nan = ' + str(chi2))    
+            
         return -0.5*chi2
 
 
@@ -393,7 +411,7 @@ if __name__ ==  "__main__":
     series_cosmo = dfcosmo.loc[simtype]
     
     
-    gridname = series_cosmo.loc['gridname']
+    gridname = series_cosmo.loc['gridname']+'morepoint_True'
     
 
     # COSMOLOGICAL GLOBALS: fiducial model (should match input sim data!)
@@ -458,7 +476,7 @@ if __name__ ==  "__main__":
     bounds = zip(bfmintab,bfmaxtab)
 
     ##### Initial guess for the b_i #####
-    inipos = np.array([2.0310343 ,  -2.62623719,  -0.39661384,   4.21514113,
+    inipos = np.array([1.85 ,  -2.62623719,  -0.39661384,   4.21514113,
          8.36786486, -29.68630616,   1.03528956, -32.39092667,
         40.00717862,   4.61905778,   100])
 
