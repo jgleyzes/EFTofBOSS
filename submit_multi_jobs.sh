dd2b349b1d0678e90ed0ab50da224f3738097fa8
kokron <nkokron@gmail.com>
2018-10-29 19:20:34 -0700

    new submission file for theory covs

--- submit_multi_jobs.sh
+++ submit_multi_jobs.sh
@@ -4,23 +4,23 @@
 #simname=ChallengeA box name is conChallengeAwideh
 #simname=LightConeHector box name is conLightConeHectorwideh
 planck=0
-simname=LightConeHector
+simname=ChallengeJapanCMASS2
 marg=1
 theoryCovness=0
 #for kmax in 0.1
-for kmax in 0
+for kmax in 0.15 0.2 0.25
 #for kmax in 0.25
 do
-#for kmaxbisp in 0.07 0.1
-for kmaxbisp in 0.07
+for kmaxbisp in 0
+#for kmaxbisp in 0.0
 do
 #for number in A B F G 
-for number in 5 8  
+for number in 1 2  
 do
  #the above is for challenge boxes
-sbatch --job-name=b$number.$kmax.$simname --output=outfiles/b$number.$kmax.$kmaxbisp.$simname.Planck$planck.out cluster_MCMC.sh $number $kmax $simname $planck conLightConeHectorwideh $kmaxbisp $marg $theoryCovness
+sbatch --job-name=b$number.$kmax.$simname --output=outfiles/b$number.$kmax.$kmaxbisp.$simname.Planck$planck.cov$theoryCovness.out cluster_MCMC.sh $number $kmax $simname $planck conLightConeDidav1.13lowAs115ns0.9652 $kmaxbisp $marg $theoryCovness
 done
-#sbatch --job-name=bdata.$kmax.$simname --output=outfiles/bdata.$kmax.$kmaxbisp.$simname.Planck$planck.out cluster_MCMC.sh data $kmax $simname $planck conLightConeHectorwideh $kmaxbisp $marg
+#sbatch --job-name=bdata.$kmax.$simname --output=outfiles/bdata.$kmax.$kmaxbisp.$simname.Planck$planck.out cluster_MCMC.sh data $kmax $simname $planck conLightConeHectorwideh $kmaxbisp $marg $theoryCovness
 done
 done
 
